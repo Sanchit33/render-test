4c746a98a0c26b38e9f8780611680e40d7ab1c39
Sanchit33 <96529417+Sanchit33@users.noreply.github.com>
2024-09-24 13:36:45 +0530

    update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/api/notes', (req,res) =>{
   res.json(note)
 })
     
-
-app.listen(3000)
-
-console.log("Server is running on port 3000")
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
